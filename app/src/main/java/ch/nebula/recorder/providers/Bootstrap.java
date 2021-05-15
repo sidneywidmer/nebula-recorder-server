d2953591062c9c503fcf8e1aeb547ea494769334
oliver <isleroli@students.zhaw.ch>
2021-05-15 16:04:03 +0200

    add mailgun dependency and MailGunProvider

--- app/src/main/java/ch/nebula/recorder/providers/Bootstrap.java
+++ app/src/main/java/ch/nebula/recorder/providers/Bootstrap.java
@@ -1,10 +1,10 @@
 package ch.nebula.recorder.providers;
 
 import com.google.inject.AbstractModule;
-import com.mitchellbosecke.pebble.PebbleEngine;
 import com.typesafe.config.Config;
 import io.ebean.Database;
 import io.javalin.Javalin;
+import net.sargue.mailgun.Configuration;
 
 import javax.validation.Validator;
 
@@ -18,5 +18,6 @@ public class Bootstrap extends AbstractModule {
         bind(Database.class).toProvider(DbProvider.class).asEagerSingleton();
         bind(Validator.class).toProvider(ValidationProvider.class).asEagerSingleton();
         bind(Javalin.class).toProvider(JavalinProvider.class).asEagerSingleton();
+        bind(Configuration.class).toProvider(MailGunProvider.class).asEagerSingleton();
     }
 }
---
+++ app/src/main/java/ch/nebula/recorder/providers/MailGunProvider.java
@@ -0,0 +1,18 @@
+package ch.nebula.recorder.providers;
+
+import net.sargue.mailgun.Configuration;
+
+import javax.inject.Provider;
+
+public class MailGunProvider implements Provider<Configuration> {
+    /**
+     * Create new MailGun Configuration.
+     */
+    @Override
+    public Configuration get() {
+        return new Configuration()
+                .domain("sandbox1791bc77d949423db055ad0edc7cb05c.mailgun.org")
+                .apiKey("c02890d811029c24498c5eb9f38391c5-2a9a428a-ac8a9c43")
+                .from("postmaster@sandbox1791bc77d949423db055ad0edc7cb05c.mailgun.or");
+    }
+}
