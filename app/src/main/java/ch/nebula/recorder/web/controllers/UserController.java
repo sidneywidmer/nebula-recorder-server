b6b4d6bc337ff5f200cbdcf28185939706b7d5b9
oliver <isleroli@students.zhaw.ch>
2021-05-15 16:51:02 +0200

    add mailservice

--- app/src/main/java/ch/nebula/recorder/web/controllers/UserController.java
+++ app/src/main/java/ch/nebula/recorder/web/controllers/UserController.java
@@ -3,8 +3,9 @@ package ch.nebula.recorder.web.controllers;
 import ch.nebula.recorder.core.exceptions.ApiException;
 import ch.nebula.recorder.core.exceptions.SystemException;
 import ch.nebula.recorder.domain.requests.UserSignupRequest;
-import io.javalin.http.Context;
+import ch.nebula.recorder.domain.services.MailService;
 import ch.nebula.recorder.domain.services.UserService;
+import io.javalin.http.Context;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -14,16 +15,19 @@ import javax.validation.Validator;
 @Singleton
 public class UserController extends BaseController {
     private final UserService userService;
+    private final MailService mailService;
 
     @Inject
-    protected UserController(UserService userService, Validator validator) {
+    protected UserController(UserService userService, Validator validator, MailService mailService) {
         super(validator);
         this.userService = userService;
+        this.mailService = mailService;
     }
 
     public void signup(Context ctx) throws ApiException, SystemException {
         var userSignup = (UserSignupRequest) this.validate(ctx, UserSignupRequest.class);
         this.userService.create(userSignup);
+        this.mailService.send();
 
         ctx.status(200);
     }
