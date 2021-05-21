1dc856cc4724e29ad0cf664b22b3c27a5565a951
oliver <isleroli@students.zhaw.ch>
2021-05-21 18:27:02 +0200

    add user as param and rename send-method of MailService

--- app/src/main/java/ch/nebula/recorder/domain/services/MailService.java
+++ app/src/main/java/ch/nebula/recorder/domain/services/MailService.java
@@ -1,5 +1,6 @@
 package ch.nebula.recorder.domain.services;
 
+import ch.nebula.recorder.domain.models.User;
 import com.google.inject.Inject;
 import net.sargue.mailgun.Configuration;
 import net.sargue.mailgun.Mail;
@@ -12,9 +13,12 @@ public class MailService {
         this.configuration = configuration;
     }
 
-    public void send() {
+    /**
+     * Try to send an activation mail to the new user by also creating an activation code.
+     */
+    public void sendActivationMail(User user) {
         Mail.using(configuration)
-                .to("oliverisler93@gmail.com")
+                .to("oliverisler93@gmail.com") // TODO: replace hard coded email with user.getEmail()
                 .subject("Activation code")
                 .text("A-123456789")
                 .build()
