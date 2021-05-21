9a6e375360a8c358a38aace39dd6c363a43eacd6
oliver <isleroli@students.zhaw.ch>
2021-05-21 15:49:56 +0200

    change reciever mail to an authorized recipient

--- app/src/main/java/ch/nebula/recorder/providers/MailGunProvider.java
+++ app/src/main/java/ch/nebula/recorder/providers/MailGunProvider.java
@@ -6,13 +6,13 @@ import javax.inject.Provider;
 
 public class MailGunProvider implements Provider<Configuration> {
     /**
-     * Create new MailGun Configuration.
+     * Create new MailGun Configuration to send mails.
      */
     @Override
     public Configuration get() {
         return new Configuration()
                 .domain("sandbox1791bc77d949423db055ad0edc7cb05c.mailgun.org")
                 .apiKey("c02890d811029c24498c5eb9f38391c5-2a9a428a-ac8a9c43")
-                .from("postmaster@sandbox1791bc77d949423db055ad0edc7cb05c.mailgun.or");
+                .from("Test account", "postmaster@sandbox1791bc77d949423db055ad0edc7cb05c.mailgun.org");
     }
 }
