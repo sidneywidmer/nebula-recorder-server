8248e9ae8388a2bc97fe8e8b1d7c2d4a0399df4a
oliver <isleroli@students.zhaw.ch>
2021-05-29 16:56:05 +0200

    create new Recording Model

---
+++ app/src/main/java/ch/nebula/recorder/domain/models/Recording.java
@@ -0,0 +1,10 @@
+package ch.nebula.recorder.domain.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "recordings")
+public class Recording extends BaseModel {
+
+}
