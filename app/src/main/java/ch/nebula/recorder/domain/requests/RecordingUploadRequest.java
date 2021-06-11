f0c5c770ad93ae0a67ff75aa7ade08ea3d715138
oliver <isleroli@students.zhaw.ch>
2021-06-11 15:43:24 +0200

    add functionality to safe a file on disk an create a recording dbo

--- app/src/main/java/ch/nebula/recorder/domain/requests/RecordingUploadRequest.java
+++ app/src/main/java/ch/nebula/recorder/domain/requests/RecordingUploadRequest.java
@@ -3,6 +3,7 @@ package ch.nebula.recorder.domain.requests;
 import ch.nebula.recorder.core.RecordingType;
 import io.ebean.annotation.NotNull;
 
+import javax.persistence.Lob;
 import javax.validation.constraints.Size;
 
 public class RecordingUploadRequest {
@@ -17,7 +18,8 @@ public class RecordingUploadRequest {
     String description;
 
     @NotNull
-    String recording;
+    @Lob
+    byte[] recording;
 
     public String getName() {
         return name;
@@ -43,11 +45,11 @@ public class RecordingUploadRequest {
         this.description = description;
     }
 
-    public String getRecording() {
+    public byte[] getRecording() {
         return recording;
     }
 
-    public void setRecording(String recording) {
+    public void setRecording(byte[] recording) {
         this.recording = recording;
     }
 }
--- app/src/main/java/ch/nebula/recorder/domain/services/RecordingService.java
+++ app/src/main/java/ch/nebula/recorder/domain/services/RecordingService.java
@@ -1,26 +1,55 @@
 package ch.nebula.recorder.domain.services;
 
+import ch.nebula.recorder.core.RecordingType;
 import ch.nebula.recorder.core.exceptions.InvalidDataException;
 import ch.nebula.recorder.domain.models.Recording;
 import ch.nebula.recorder.domain.models.User;
 import ch.nebula.recorder.domain.requests.RecordingUploadRequest;
+import com.typesafe.config.Config;
 
+import javax.inject.Inject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class RecordingService {
+    private final Config config;
+
+    @Inject
+    public RecordingService(Config config) {
+        this.config = config;
+    }
 
     /**
      * After a user finished a recording on the client it can be uploaded to the server.
+     * Uploaded means the file is written on a filesystem and a recording dbo is generated.
      */
     public void upload(User user, RecordingUploadRequest recordingUploadRequest) throws InvalidDataException {
         if (user == null) {
             throw new InvalidDataException(Map.of("-", "Illegal State"));
         }
 
-        String name = user.getId() + "-" + recordingUploadRequest.getName();
-        var recording = new Recording(name, recordingUploadRequest.getType(), user);
+        var name = user.getId() + "-" + recordingUploadRequest.getName();
+        var data = recordingUploadRequest.getRecording();
+        write(name, data);
 
-        String description = recordingUploadRequest.getDescription();
+        var description = recordingUploadRequest.getDescription();
+        var recordingType = recordingUploadRequest.getType();
+        create(name, recordingType, description, user);
+    }
+
+    private void write(String name, byte[] data) throws InvalidDataException {
+        try {
+            Files.write(Paths.get(config.getString("file.path"), name), data);
+        } catch (IOException e) {
+            throw new InvalidDataException(Map.of("-", ""));
+        }
+    }
+
+    private void create(String name, RecordingType recordingType, String description, User user) {
+        var recording = new Recording(name, recordingType, user);
+
         if (description != null && !"".equals(description)) {
             recording.setDescription(description);
         }
