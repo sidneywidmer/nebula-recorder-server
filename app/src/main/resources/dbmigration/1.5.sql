c900b4b10f5787a438794d61e92fa626349a5a82
oliver <isleroli@students.zhaw.ch>
2021-06-11 14:10:20 +0200

    remove increments 1.4 and 1.5

--- app/src/main/resources/dbmigration/1.5.sql
+++
@@ -1,7 +0,0 @@
--- apply changes
-alter table recordings alter column name type varchar(30) using name::varchar(30);
-alter table recordings add column recording_type integer not null;
-alter table recordings add constraint ck_recordings_recording_type check ( recording_type in (0,1,2));
-alter table recordings add column tag varchar(30);
-alter table recordings add column description varchar(50);
-
