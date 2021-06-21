48b51e9475d786ff40d4a3537e14309d8ec3728a
Sidney <hello@sidney.dev>
2021-06-21 16:38:42 +0200

    Fix FE API routes and email confirmatio config

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -32,7 +32,7 @@ export default new Vuex.Store({
             return new Promise((resolve, reject) => {
                 commit("auth_request");
                 axios({
-                    url: "http://localhost:8000/api/auth/login",
+                    url: "/api/auth/login",
                     data: user,
                     method: "POST"
                 })
@@ -54,7 +54,7 @@ export default new Vuex.Store({
         register({commit}, user) {
             return new Promise((resolve, reject) => {
                 commit('auth_request')
-                axios({url: 'http://localhost:8000/api/user/signup', data: user, method: 'POST'})
+                axios({url: '/api/user/signup', data: user, method: 'POST'})
                     .then(resp => {
                         /**
                          * do success
