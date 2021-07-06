a39e0df9ad36b4cf97ccabb6ce9513348e664910
Sidney <hello@sidney.dev>
2021-07-06 21:39:52 +0200

    Basic json response and parsing of newly uploaded recording

--- app/src/main/java/ch/nebula/recorder/domain/services/RecordingService.java
+++ app/src/main/java/ch/nebula/recorder/domain/services/RecordingService.java
@@ -52,41 +52,22 @@ public class RecordingService {
     }
 
     /**
-     * If a user wants to see all his uploaded recordings this method generates a JSON of all available recordings by
-     * validating if the passed user id exists.
+     * Get all recordings of given user.
      */
-    public String getAll(User user) throws RecordingNotFoundException {
-        List<Recording> recordings = new QRecording().user.equalTo(user).findList();
-
-        var jsonArray = new JSONArray();
-        for (Recording recording : recordings) {
-            var jsonObject = new JSONObject();
-            jsonObject.put("id", recording.getId());
-            jsonObject.put("name", recording.getName());
-            jsonObject.put("url", url(recording));
-
-            jsonArray.put(jsonObject);
-        }
-
-        return jsonArray.toString(1);
+    public List<Recording> getAll(User user) throws RecordingNotFoundException {
+        return new QRecording().user.equalTo(user).findList();
     }
 
     /**
-     * If a user wants so see a specific recording this method generates a JSON of the specific recording if it is
-     * available.
+     * Get one specific recording by id.
      */
-    public String getOne(long id) throws RecordingNotFoundException {
+    public Recording getOne(long id) throws RecordingNotFoundException {
         var recording = new QRecording().id.equalTo(id).findOne();
         if (recording == null) {
             throw new RecordingNotFoundException(String.format("Recording with id: %d not found.", id));
         }
 
-        var jsonObject = new JSONObject();
-        jsonObject.put("id", recording.getId());
-        jsonObject.put("name", recording.getName());
-        jsonObject.put("url", url(recording));
-
-        return jsonObject.toString(1);
+        return recording;
     }
 
     private void write(String name, UploadedFile data) {
@@ -106,7 +87,4 @@ public class RecordingService {
         return recording;
     }
 
-    private String url(Recording recording) {
-        return "/recordings/" + recording.getName();
-    }
 }
